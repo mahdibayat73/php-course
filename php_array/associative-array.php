4df92d510e80bcb99fb0acfa4a26066720f4dba0
mahdibayat73 <mahdi.riraco@gmail.com>
2023-07-11 14:05:38 +0330

    Add PHP Array Folder

---
+++ php_array/associative-array.php
@@ -0,0 +1,9 @@
+<?php 
+$age = array(
+    'Mahdi' => 28,
+    'Javad' => 23,
+    'Ali' => 27,
+);
+print_r($age);
+echo '<br>';
+echo 'This is Mahdi\'s age: ' . $age['Mahdi'];
---
+++ php_array/indexed-array.php
@@ -0,0 +1,6 @@
+<?php 
+
+$cars = array('BMW', 'Toyota', 'Porsche');
+print_r($cars);
+echo '<br>';
+echo 'This is the second member of the array: ' . $cars[2];
