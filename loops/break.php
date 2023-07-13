e0af908975378dcefdfc6d5aa634d3badc3e1cdc
mahdibayat73 <mahdi.riraco@gmail.com>
2023-07-13 19:53:43 +0330

    Add PHP Loops Folder

---
+++ loops/break.php
@@ -0,0 +1,8 @@
+<?php 
+
+for ( $i = 0; $i <= 9; $i++ ) {
+    if ( $i == 7 ) {
+        break;
+    }
+    echo $i . '<br>';
+}
---
+++ loops/continue.php
@@ -0,0 +1,8 @@
+<?php 
+
+for ( $i = 0; $i <= 9; $i++ ) {
+    if ( $i == 5 ) {
+        continue;
+    }
+    echo $i . '<br>';
+}
