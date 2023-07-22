c68eab48a96317d08d3271008f96b847cdcfea73
mahdibayat73 <mahdi.riraco@gmail.com>
2023-07-22 21:02:03 +0330

    Add 01-switch File

---
+++ php_switch/01-switch.php
@@ -0,0 +1,16 @@
+<?php 
+$num = 369;
+
+switch ( $num ) {
+    case 3:
+        echo "number is equals to 3";
+    break;
+    case 6:
+        echo "number is equals to 6";
+    break;
+    case 9:
+        echo "number is equals to 9";
+    break;
+    default:
+        echo "number is equals to 369";
+}
