8656741507c3a2551fcd83c96c95607aabad0e17
mahdibayat73 <mahdi.riraco@gmail.com>
2023-11-02 11:13:42 +0330

    Add Date & Time Folder

---
+++ php_date_time/01-date-time.php
@@ -0,0 +1,8 @@
+<?php 
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("l") . "<br>";
+echo "<hr>";
+?>
+
+© 2010-<?php echo date("Y");?>
---
+++ php_date_time/02-date-time.php
@@ -0,0 +1,8 @@
+<?php 
+date_default_timezone_set("Asia/Tehran");
+echo "The Time is: " . date("H") . "<br>";
+echo "The Time is: " . date("h") . "<br>";
+echo "The Minutes is: " . date("i") . "<br>";
+echo "The Second is: " . date("s") . "<br>";
+echo "<hr>";
+echo "The Time is: " . date("H:i:s A");
