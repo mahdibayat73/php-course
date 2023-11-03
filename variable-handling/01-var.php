344e535a5c125572cb0d4bb480dc9b9870e6cc50
mahdibayat73 <mahdi.riraco@gmail.com>
2023-11-03 18:22:59 +0330

    Add PHP  Variable Handling Folder

---
+++ variable-handling/01-var.php
@@ -0,0 +1,21 @@
+<?php 
+$a = "Mahdi";
+echo gettype($a) . "<br>";
+
+$b = 369;
+echo gettype($b) . "<br>";
+
+$c = "36.9";
+echo gettype($c) . "<br>";
+
+$d = array();
+echo gettype($d) . "<br>";
+
+$e = array("red", "green", "blue");
+echo gettype($e) . "<br>";
+
+$f = NULL;
+echo gettype($f) . "<br>";
+
+$g = false;
+echo gettype($g) . "<br>";
---
+++ variable-handling/02-empty&isset.php
@@ -0,0 +1,10 @@
+<?php 
+$a = '';
+
+if ( empty($a) ) {
+    echo "Variable 'a' is empty.<br>";
+}
+
+if ( isset($a) ) {
+    echo "Variable 'a' is set.";
+}
