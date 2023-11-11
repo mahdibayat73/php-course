9914c5b3450ddf78bad859d589f1f5112ca2c3f5
mahdibayat73 <mahdi.riraco@gmail.com>
2023-11-11 21:00:07 +0330

    Added 3exercises about Switch

---
+++ conditions/09-condition.php
@@ -0,0 +1,12 @@
+<?php
+$favcolor = "red";
+
+if ( $favcolor == "red" ) {
+    echo "Your favorite color is red!";
+}elseif ( $favcolor == "blue" ) {
+    echo "Your favorite color is blue!";
+}elseif ( $favcolor == "green" ) {
+    echo "Your favorite color is green!";
+}else {
+    echo "Your favorite color is neither red, blue, nor green!";
+}
---
+++ conditions/11-condition.php
@@ -0,0 +1,13 @@
+<?php
+$favcolor = "red";
+
+switch ($favcolor) {
+    case "red":
+        echo "Your favorite color is red! <br>";
+    case "blue":
+        echo "Your favorite color is blue! <br>";
+    case "green":
+        echo "Your favorite color is green! <br>";
+    default:
+        echo "Your favorite color is neither red, blue, nor green!";
+}
