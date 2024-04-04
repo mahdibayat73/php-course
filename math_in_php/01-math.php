530a79bb2c07c259400a78e5b3f3bd145b733c3d
mahdibayat73 <mahdi.riraco@gmail.com>
2024-04-04 14:12:42 +0330

    Practice Math in PHP

---
+++ math_in_php/01-math.php
@@ -0,0 +1,10 @@
+<?php 
+
+// PHP round() Function
+// The round() function rounds a floating-point number to its nearest integer:
+
+echo round(0.66) . "<br>";
+echo round(0.50) . "<br>";
+echo round(0.49) . "<br>";
+echo round(-4.40) . "<br>";
+echo round(-4.60) . "<br>";
---
+++ math_in_php/02-math.php
@@ -0,0 +1,6 @@
+<?php 
+
+// Random Numbers
+// The rand() function generates a random number:
+
+echo rand();
