3441fe1bf3070f5a0d40cc07ca136ce3f599c076
mahdibayat73 <mahdi.riraco@gmail.com>
2024-04-05 15:08:24 +0330

    Practice constants in PHP

---
+++ constants/01-constants.php
@@ -0,0 +1,11 @@
+<?php 
+
+// PHP Constants
+// A constant is an identifier (name) for a simple value. The value cannot be changed or undefined.
+// A valid constant name starts with a letter or underscore (no $ sign before the constant name).
+// NoteL Unlike variables, constants are auyomatically global across the entire script.
+
+# define( name, value, case-insensitive );
+
+define( "domainName", "https://frontenddesign.ir/" );
+echo domainName;
