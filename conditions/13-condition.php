0a2d44afcd3893d8099c7cd83c2e9d070a516506
mahdibayat73 <mahdi.riraco@gmail.com>
2024-04-09 13:06:16 +0330

    Practice Conditions in PHP

---
+++ conditions/13-condition.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo "<h1>PHP if Statements</h1>";
+echo "<p>onditional statements are used to performdifferent actions diffrent conditions.</p>";
+echo "<h2>PHP Conditional Statements</h2>";
+echo "<p>Very often when you write code, you want to perform different actions for different for different conditions. You can use conditional statements in your code to do this.</p>";
+echo "<p>In PHP we have the following conditional statements:</p>";
+echo "<ul>";
+echo "<li><strong>if</strong> statement - executes some code if on condition is true</li>";
+echo "<li><strong>if...else</strong> statement - executes some code if a condition is true and another code if that condition is false</li>";
+echo "<li><strong>if...elseif...else</strong> statement - executes different codes for more than two conditions</li>";
+echo "<li><strong>switch</strong> statement - selects on of many blocks of code to be executed</li>";
+echo "</ul>";
---
+++ conditions/14-condition.php
@@ -0,0 +1,7 @@
+<?php 
+
+// Output "Have a good day!" if 5 is lager than 3:
+
+if ( 5 > 3 ) {
+    echo "Have a good day!";
+}
