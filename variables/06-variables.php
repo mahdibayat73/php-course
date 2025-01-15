bcd80b6165a683d538dbc58699541c8d6ae8ce6a
mahdibayat73 <mahdi.riraco@gmail.com>
2025-01-15 09:25:19 +0330

    Practice variables in PHP

---
+++ variables/06-variables.php
@@ -0,0 +1,24 @@
+<?php 
+
+$string = "Mahdi Bayat";
+var_dump($string);
+echo "<hr>";
+$number = 369;
+var_dump($number);
+echo "<hr>";
+$float = 36.9;
+var_dump($float);
+echo "<hr>";
+$boolean = true;
+var_dump($boolean);
+echo "<hr>";
+$array = [
+    "Name" => "Mahdi",
+    "Age" => 29,
+    "single" => true
+];
+var_dump($array);
+echo "<hr>";
+echo "<pre>";
+print_r($array);
+echo "</pre>";
