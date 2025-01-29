5befc74daf6ff72dd57c384995e0b1a5f0ff0e95
mahdibayat73 <mahdi.riraco@gmail.com>
2025-01-29 20:27:27 +0330

    PHP Practice

---
+++ practice/03-practice.php
@@ -0,0 +1,13 @@
+<?php 
+
+for ($i = 1; $i <= 10; $i++) {
+    echo "عدد: $i <br>";
+}
+
+echo "<hr>";
+
+$j = 1; // مقدار اولیه برای while
+while ($j <= 10) {
+    echo "<strong>عدد:</strong> $j <br>";
+    $j++;
+}
